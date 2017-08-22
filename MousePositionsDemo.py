23ca2c2fb3da9d6e416210aca39d5db09f1d6eb0
eswaribala <Parameswaribala@gmail.com>
2017-08-22 14:35:30 +0530

    Add files via upload

---
+++ MousePositionsDemo.py
@@ -0,0 +1,18 @@
+'''
+Created on 21-Aug-2017
+
+@author: BALASUBRAMANIAM
+'''
+import pyautogui
+#mouse movement in square pattern
+for i in range(10):
+    pyautogui.moveTo(100, 100, duration=0.25)
+    pyautogui.moveTo(200, 100, duration=0.25)
+    pyautogui.moveTo(200, 200, duration=0.25)
+    pyautogui.moveTo(100, 200, duration=0.25)
+
+for i in range(10):
+    pyautogui.moveRel(100, 0, duration=0.25)
+    pyautogui.moveRel(0, 100, duration=0.25)
+    pyautogui.moveRel(-100, 0, duration=0.25)
+    pyautogui.moveRel(0, -100, duration=0.25)

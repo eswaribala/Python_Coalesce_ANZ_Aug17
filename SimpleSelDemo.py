b1133b198901402046ac45c8a11c74ef961cc6ea
eswaribala <Parameswaribala@gmail.com>
2017-08-22 11:39:08 +0530

    Add files via upload

---
+++ SimpleSelDemo.py
@@ -0,0 +1,17 @@
+'''
+Created on 21-Aug-2017
+
+@author: BALASUBRAMANIAM
+'''
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Edge()
+driver.get("https://www.python.org/")
+assert "Python" in driver.title
+elem = driver.find_element_by_name("q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+driver.close()

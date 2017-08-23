5aed209ae4e4600da686be4e301842cdae9b05c8
eswaribala <Parameswaribala@gmail.com>
2017-08-23 15:31:25 +0530

    Add files via upload

---
+++ ExceptionHandlingDemo.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 20 22:34:37 2017
+
+@author: BALASUBRAMANIAM
+"""
+# import module sys to get the type of exception
+import sys
+
+randomList = ['a', 0, 2]
+
+for entry in randomList:
+    try:
+        print("The entry is", entry)
+        r = 1/int(entry)
+        break
+    except:
+        print("Oops!",sys.exc_info()[0],"occured.")
+        print("Next entry.")
+        print()
+print("The reciprocal of",entry,"is",r)
+
+while True:
+    try:
+        n = input("Please enter an integer: ")
+        n = int(n)
+        break
+    except ValueError:
+        print("No valid integer! Please try again ...")
+print ("Great, you successfully entered an integer!")
+
+import sys
+
+try:
+    f = open('integers.txt')
+    s = f.readline()
+    i = int(s.strip())
+except IOError as errno:
+    print ("I/O error({0})".format(errno))
+except ValueError:
+    print ("No valid integer in line.")
+except:
+    print ("Unexpected error:", sys.exc_info()[0])
+    raise
+
+# define Python user-defined exceptions
+class Error(Exception):
+   """Base class for other exceptions"""
+   pass
+
+class ValueTooSmallError(Error):
+   """Raised when the input value is too small"""
+   pass
+
+class ValueTooLargeError(Error):
+   """Raised when the input value is too large"""
+   pass
+
+# our main program
+# user guesses a number until he/she gets it right
+
+# you need to guess this number
+number = 10
+
+while True:
+   try:
+       i_num = int(input("Enter a number: "))
+       if i_num < number:
+           raise ValueTooSmallError
+       elif i_num > number:
+           raise ValueTooLargeError
+       break
+   except ValueTooSmallError:
+       print("This value is too small, try again!")
+       print()
+   except ValueTooLargeError:
+       print("This value is too large, try again!")
+       print()
+
+print("Congratulations! You guessed it correctly.")
+
